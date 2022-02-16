26a70d3d9f3a253eb63e0fae5bb21708787c4235
Sharifa Mohamed <mhmdshari@gmail.com>
2022-02-16 14:18:53 +0530

    authentication added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
+import { Authenticator } from '@aws-amplify/ui-react';
+import '@aws-amplify/ui-react/styles.css';
+import awsconfig from './aws-exports'
+import Amplify from 'aws-amplify'
+
+Amplify.configure(awsconfig);
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h2>Hello AWS!!!</h2>
-      </header>
-    </div>
+    <Authenticator>
+      {({ signOut, user }) => (
+        <div className="App">
+          <p>
+            Hey {user.username}, welcome to my channel, with auth!
+          </p>
+          <button onClick={signOut}>Sign out</button>
+
+        </div>
+      )}
+    </Authenticator>
   );
 }
 
